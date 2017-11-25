00b3704119e144557910b524a54894625ed24203
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2017-11-25 11:31:35 +0700

    Add findContour

--- src/club/hcmiuiot/vnlpr/LPRengine.java
+++ src/club/hcmiuiot/vnlpr/LPRengine.java
@@ -1,6 +1,7 @@
 package club.hcmiuiot.vnlpr;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
@@ -12,7 +13,7 @@ public class LPRengine {
 	
 	public static void main(String[] args) {
 		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-		Mat src = Imgcodecs.imread("img/bike/7.jpg");
+		Mat src = Imgcodecs.imread("img/bike/0.jpg");
 		//Debug.imshow("Source", src);
 		
 		Mat gray = new Mat();
@@ -23,6 +24,8 @@ public class LPRengine {
 		mini_src = gray;
 		//Imgproc.pyrDown(gray, mini_src, new Size(src.cols() / scale, src.rows() / scale));
 		//Debug.imshow("PyDowned", mini_src);
+		//Imgproc.blur(mini_src, mini_src, new Size(1,1));
+		//Debug.imshow("Mini Src Blured", mini_src);
 		
 		Imgproc.adaptiveThreshold(mini_src, mini_src, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 51, -10);
 		Debug.imshow("AdaptiveThreshold", mini_src);
@@ -34,9 +37,9 @@ public class LPRengine {
 		
 		Mat dilated = new Mat();
 		Mat eroded = new Mat(); 
-		Imgproc.erode(mini_src, eroded, se2);
+		Imgproc.erode(mini_src, eroded, se1);
 		Debug.imshow("Erode", eroded);
-		Imgproc.dilate(eroded, dilated, se1);
+		Imgproc.dilate(eroded, dilated, se2);
 		Debug.imshow("Dilate", dilated);
 		//Imgproc.erode(dilated, dilated, se2);
 		//Debug.imshow("Erode2", dilated);
@@ -44,13 +47,27 @@ public class LPRengine {
 		ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 		Mat hierachy = new Mat();
 		
-		Imgproc.findContours(dilated, contours, hierachy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+		Imgproc.findContours(dilated, contours, hierachy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
 		
 		for (int i = 0; i < contours.size(); i++) {
+			//hasChild
 			Rect rec = Imgproc.boundingRect(contours.get(i));
+			//RotatedRect rotatedRect = Imgproc.fitEllipse((MatOfPoint2f)(contours.get[i]));
 			float ratio = 1.0f * rec.width / rec.height;
-			if (ratio >= 1.3f && ratio <= 1.7f)
-				Imgproc.rectangle(src, rec.tl(), rec.br(), new Scalar(255,0,0));
+			//hierachy.
+			//if (hierachy.get(i, 3)[1] != -1) {
+				//Imgproc.rectangle(src, rec.tl(), rec.br(), new Scalar(255,0,0));
+			//}
+				//Mat roi = new Mat(src, rec);
+				//if (ratio >= 0.42 && ratio <= 0.49 && rec.area() > 500) {
+			if (rec.area()>200)
+					Imgproc.rectangle(src, rec.tl(), rec.br(), new Scalar(255,0,0));
+					//Debug.imshow(""+i, sr);
+				//}
+				
+			//Imgproc.drawContours(src, contours, i, new Scalar(0,0,255));
+			//if (ratio >= 1.3f && ratio <= 1.7f)
+				//Imgproc.rectangle(src, rec.tl(), rec.br(), new Scalar(255,0,0));
 		}
 		Debug.imshow("Contours", src);
 		
