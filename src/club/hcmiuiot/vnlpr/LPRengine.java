548e3f8d991ee44928807a69931277fcc4b41317
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2017-11-26 10:19:51 +0700

    Add FPS test code

--- src/club/hcmiuiot/vnlpr/LPRengine.java
+++ src/club/hcmiuiot/vnlpr/LPRengine.java
@@ -2,6 +2,7 @@ package club.hcmiuiot.vnlpr;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import org.opencv.core.*;
 import org.opencv.imgcodecs.*;
@@ -22,15 +23,29 @@ public class LPRengine {
 		LPRengine.val1 = val1;
 	}
 
-
 	public static void main(String[] args) {
 		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
 //		VideoCapture vc = new VideoCapture(0);
 //		BackgroundSubtractor bs = Video.createBackgroundSubtractorMOG2();
-	
 		boolean b = true;
+//		Mat frame = new Mat();
+//		Mat a = new Mat();
+//		long timeStart, timeEnd;
+//		double interval;
+//		int fps=0;
+//		while (b) {
+//			timeStart = System.nanoTime();
+//			vc.read(frame);
+//			Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
+//			Imgproc.adaptiveThreshold(frame, a, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 155, -10);
+//			timeEnd = System.nanoTime();
+//			interval = (timeEnd - timeStart);
+//			fps = (int) (1000000000f/interval);
+//			Imgproc.putText(frame, "FPS "+ fps, new Point(10,50), 0, 1, new Scalar(255,0,0));
+//			Debug.imshow("FPS", a);
+//		}
 			
-		Mat src = Imgcodecs.imread("img/bike/0.jpg");
+		Mat src = Imgcodecs.imread("img/bike/3b.jpg");
 		//Debug.imshow("Source", src);
 		
 		Mat gray = new Mat();
@@ -41,23 +56,16 @@ public class LPRengine {
 		mini_src = gray.clone();
 		//Imgproc.pyrDown(gray, mini_src, new Size(src.cols() / scale, src.rows() / scale));
 		//Debug.imshow("PyDowned", mini_src);
-		//Imgproc.blur(mini_src, mini_src, new Size(1,1));
-		//Debug.imshow("Mini Src Blured", mini_src);
-		//Imgproc.morphologyEx(mini_src, mini_src, Imgproc.MORPH_BLACKHAT, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(20,20)));
-		//Debug.imshow("Normalized", mini_src);
-		//Core.normalize(mini_src, mini_src, 3, 255, Core.NORM_HAMMING);
-		//Imgproc.threshold(mini_src, mini_src, Core.mean(mini_src).val[0]*10, 255, Imgproc.THRESH_BINARY);
-		//Imgproc.adaptiveThreshold(mini_src, mini_src, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 51, -10);
 		
 		Debug.imshow("Normalized2", mini_src);
 
 		Mat debug = mini_src.clone();
-		//Core.normalize(mini_src, mini_src, 0, 255, Core.NORM_MINMAX);
 		
-		Thread slider1 = new Thread(new Slider(val1));
-		slider1.start();
+		//Thread slider1 = new Thread(new Slider(val1));
+		//slider1.start();
 		
-		while (b) {
+		//while (b) {
+		val1 = 55;
 			if (val1>1 && val1%2==1) {
 				Imgproc.adaptiveThreshold(mini_src, debug, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, val1, -10);
 				Imgproc.erode(debug, debug, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,2)));
@@ -68,13 +76,33 @@ public class LPRengine {
 			Mat hierarchy = new Mat();
 			Mat cp = src.clone();
 			Imgproc.findContours(debug, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-			for (int i=0; i<contours.size(); i++)
-				if (Imgproc.boundingRect(contours.get(i)).area() > 50) {
-					Rect rec = Imgproc.boundingRect(contours.get(i));
+			for (int i=0; i<contours.size(); i++) {
+				Rect rec = Imgproc.boundingRect(contours.get(i));
+				float ratio = 1.0f * rec.width / rec.height;
+				if (rec.area()>1000 && (ratio > 1.3f && ratio < 1.6f)) {
 					Imgproc.rectangle(cp, rec.tl(), rec.br(), new Scalar(0,0,255));
+					Mat plate = new Mat(debug, rec);
+					Debug.imshow("Plate "+i, plate);
+					
+					
+					ArrayList<MatOfPoint> pContours = new ArrayList<>();
+					Mat pHierarchy = new Mat();
+					Imgproc.findContours(plate, pContours, pHierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_NONE);
+					
+					int charFoundNum = 0;
+					Mat plateSrc = new Mat(src, rec);
+					for (int j=0; j<pContours.size(); j++) {
+						Rect recChar = Imgproc.boundingRect(pContours.get(j));
+						
+						if (recChar.area()>100)
+							charFoundNum++;
+						Imgproc.rectangle(plateSrc, recChar.tl(), recChar.br(), new Scalar(0,0,255),2);
+					}
+					Debug.imshow("Plate"+i, plateSrc);
 				}
-			Debug.imshow("SRC", cp);
-		}
+			}
+			//Debug.imshow("SRC", cp);
+		//}
 			
 		//Imgproc.adaptiveThreshold(mini_src, mini_src, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 201, -50);
 		//Debug.imshow("AdaptiveThreshold", mini_src);
@@ -84,49 +112,5 @@ public class LPRengine {
 //		Mat se1 = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,2));
 //		Mat se2 = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3,3));
 //		
-//		Mat dilated = new Mat();
-//		Mat eroded = new Mat(); 
-//		Imgproc.erode(mini_src, eroded, se1);
-//		Debug.imshow("Erode", eroded);
-//		Imgproc.dilate(eroded, dilated, se2);
-//		Debug.imshow("Dilate", dilated);
-//		//Imgproc.erode(dilated, dilated, se2);
-//		//Debug.imshow("Erode2", dilated);
-//		
-//		ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-//		Mat hierachy = new Mat();
-//		
-//		Imgproc.findContours(dilated, contours, hierachy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-//		
-//		for (int i = 0; i < contours.size(); i++) {
-//			//hasChild
-//			Rect rec = Imgproc.boundingRect(contours.get(i));
-//			//RotatedRect rotatedRect = Imgproc.fitEllipse((MatOfPoint2f)(contours.get[i]));
-//			float ratio = 1.0f * rec.width / rec.height;
-//			//hierachy.
-//			//if (hierachy.get(i, 3)[1] != -1) {
-//				//Imgproc.rectangle(src, rec.tl(), rec.br(), new Scalar(255,0,0));
-//			//}
-//				//Mat roi = new Mat(src, rec);
-//				//if (ratio >= 0.42 && ratio <= 0.49 && rec.area() > 500) {
-//			if (rec.area()>200)
-//					Imgproc.rectangle(src, rec.tl(), rec.br(), new Scalar(255,0,0));
-//					//Debug.imshow(""+i, sr);
-//				//}
-//				
-//			//Imgproc.drawContours(src, contours, i, new Scalar(0,0,255));
-//			//if (ratio >= 1.3f && ratio <= 1.7f)
-//				//Imgproc.rectangle(src, rec.tl(), rec.br(), new Scalar(255,0,0));
-//		}
-//		Debug.imshow("Contours", src);
-		
-	//	Imgproc.rectangle(mini_src, new Point(0, 0), new Point(32*5, 32), new Scalar(255, 255, 255));	
-		//Debug.imshow("Mini source", mini_src);
-		
-	
-		
-	
-		
-		
 	}
 }
